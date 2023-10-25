707bb21e353a6328da6d653b0478c0ac8d969907
SHIVAM <shivampatel0700@gmail.com>
2023-10-25 18:16:53 +0530

    Getting data from SQL.

--- lib/models/place_model.dart
+++ lib/models/place_model.dart
@@ -4,8 +4,13 @@ import 'package:uuid/uuid.dart';
 
 var uuid = Uuid();
 
-class PlaceModel{
-  PlaceModel({required this.title, required this.image, required this.location}) : id = uuid.v4() ;
+class PlaceModel {
+  PlaceModel({
+    required this.title,
+    required this.image,
+    required this.location,
+    String? id
+  }) : id = id ?? uuid.v4();
 
   final String id;
   final String title;
--- lib/providers/add_list_provider.dart
+++ lib/providers/add_list_provider.dart
@@ -7,8 +7,38 @@ import 'package:path/path.dart' as path;
 import 'package:sqflite/sqflite.dart' as sql;
 import 'package:sqflite/sqlite_api.dart';
 
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  // it will go to database and create the data base
+  // then if database create first time, then it will onCreate
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      // this function is allow to do some initial work // db gives the access to the database
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, location TEXT)'); // we will use REAL for decimal values
+    },
+    version: 1,
+  );
+  return db;
+}
+
 class PlaceNotifier extends StateNotifier<List<PlaceModel>> {
   PlaceNotifier() : super([]);
+
+  void loadPlace() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places = data.map(
+          (row) =>
+          PlaceModel(
+              id: row['id'] as String,
+              title: row['title'] as String,
+              image: File(row['image'] as String),
+              location: row['location'] as String),
+    ).toList();
+    state = places;
+  }
 
   void addPlace(String title, File image, String location) async {
     final appDir = await syspaths.getApplicationDocumentsDirectory();
@@ -16,25 +46,15 @@ class PlaceNotifier extends StateNotifier<List<PlaceModel>> {
     final copiedImage = await image.copy('${appDir.path}/$fileName');
 
     final newPlace =
-        PlaceModel(title: title, image: copiedImage, location: location);
+    PlaceModel(title: title, image: copiedImage, location: location);
 
-    final dbPath = await sql.getDatabasesPath();
-    // it will go to database and create the data base
-    // then if database create first time, then it will onCreate
-    final db = await sql.openDatabase(
-      path.join(dbPath, 'places.db'),
-      onCreate: (db, version) {
-        // this function is allow to do some initial work // db gives the access to the database
-        return db.execute(
-            'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, location TEXT)'); // we will use REAL for decimal values
-      },
-      version: 1,
-    );
+    final db = await _getDatabase();
+
     db.insert('user_places', {
-      'id' : newPlace.id,
-      'title' : newPlace.title,
-      'image' : newPlace.image,
-      'location' : newPlace.location
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image,
+      'location': newPlace.location
     });
 
     state = [newPlace, ...state];
@@ -42,7 +62,7 @@ class PlaceNotifier extends StateNotifier<List<PlaceModel>> {
 }
 
 final placeProvider = StateNotifierProvider<PlaceNotifier, List<PlaceModel>>(
-  (ref) {
+      (ref) {
     return PlaceNotifier();
   },
 );
